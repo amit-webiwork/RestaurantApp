8236938f2150ba55c97890fe2d606ebc78afe67c
amit.maskare <amit.maskare@webiwork.com>
2024-11-19 18:47:48 +0530

    changes

--- src/components/order/PastOrderTab.tsx
+++ src/components/order/PastOrderTab.tsx
@@ -107,7 +107,13 @@ function PastOrderTab({ HeaderComponent, switchTab, activeTab, navigation }: Pro
                 refreshing={isRefreshing}
                 showsVerticalScrollIndicator={false}
                 data={pastOrders}
-                renderItem={({ item, index }) => <PastOrderItemSection item={item} index={index} navigation={navigation} deleteHandler={deleteOrderHandler} setLoader={setLoader} />}
+                renderItem={({ item, index }) => <PastOrderItemSection
+                    item={item}
+                    index={index}
+                    navigation={navigation}
+                    deleteHandler={deleteOrderHandler}
+                    setLoader={setLoader}
+                />}
                 contentContainerStyle={{
                     gap: HP(18)
                 }}
