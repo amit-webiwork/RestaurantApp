eb4182eba7856baac231dbe1878c33f20c7e16b8
amit.maskare <amit.maskare@webiwork.com>
2024-12-11 11:13:58 +0530

    changes sh1 key in json file

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -15,24 +15,24 @@ export const COLORS = {
     NAV_ICON_BG: "#DADADA"
 };
 
-export const APP_VERSION = '0.0.1';
+export const APP_VERSION = '0.0.2';
 export const OTP_SEND_WAIT_TIME = 60; // in seconds
 export const STD_CODE = "+61";
 
 export const pinCheck = /^[0-9]{4}$/;
 
-// export const BACKEND_URL = "https://api.deloungecafe.com/api/v1";
+export const BACKEND_URL = "https://api.deloungecafe.com/api/v1";
 // export const BACKEND_URL = "https://40b9-125-99-173-186.ngrok-free.app/api/v1";
-export const BACKEND_URL = "https://current-fish-manually.ngrok-free.app/api/v1";
+// export const BACKEND_URL = "https://current-fish-manually.ngrok-free.app/api/v1";
 // export const BACKEND_URL = "https://4844-125-99-173-186.ngrok-free.app/api/v1";
 
 export const CDN_URL = "https://cdn.deloungecafe.com/";
 
 // local key
-export const STRIPE_PUBLIC_KEY = "pk_test_51QCxBJHliBFhQl5Sfxyactla0vrLw2IO7t4pUFBwHywPjeap3iBS7sEe7bncI6NBdCoetAhdfIUxWGjuajVkJ6F4004SKA6sQ9";
+// export const STRIPE_PUBLIC_KEY = "pk_test_51QCxBJHliBFhQl5Sfxyactla0vrLw2IO7t4pUFBwHywPjeap3iBS7sEe7bncI6NBdCoetAhdfIUxWGjuajVkJ6F4004SKA6sQ9";
 
 // server key
-// export const STRIPE_PUBLIC_KEY = "pk_test_51QEJaCEQOrAyyyQ3QvZhdpdWJ0QfJKOb5YBRLjeQ4PNNxNoveBiPf2AS5tqA3Fih2sauafz3sz9CawNcNYcXpxen00NJBglpsa";
+export const STRIPE_PUBLIC_KEY = "pk_test_51QEJaCEQOrAyyyQ3QvZhdpdWJ0QfJKOb5YBRLjeQ4PNNxNoveBiPf2AS5tqA3Fih2sauafz3sz9CawNcNYcXpxen00NJBglpsa";
 
 export const apiEndpoints = {
     signup: "/auth/user/sign-up",
